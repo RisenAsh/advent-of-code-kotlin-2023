46e719fdf45d2cc51f806392bd0a676201ec63df
Sam Farndale <sam.farndale@autotrader.co.uk>
2023-12-02 00:09:15 +0000

    Day 1

--- src/Day01.kt
+++ src/Day01.kt
@@ -1,17 +1,67 @@
 fun main() {
+    fun formatValue(string: String): Int {
+        var value = string
+
+        value = value.filter { it.isDigit() }
+        when {
+            value.length < 2 -> {
+                value += value
+            }
+            value.length > 2 -> {
+                value = "${value.first()}${value.last()}"
+            }
+        }
+
+        return value.toInt()
+    }
+
     fun part1(input: List<String>): Int {
-        return input.size
+        val inputDigits: MutableList<Int> = mutableListOf()
+
+        input.forEach { line ->
+            inputDigits.add(formatValue(line))
+        }
+
+        return inputDigits.sum()
     }
 
     fun part2(input: List<String>): Int {
-        return input.size
+        val inputDigits: MutableList<Int> = mutableListOf()
+        val digits =
+            mapOf(
+                "one" to "o1e",
+                "two" to "t2o",
+                "three" to "t3e",
+                "four" to "f4r",
+                "five" to "f5e",
+                "six" to "s6x",
+                "seven" to "s7n",
+                "eight" to "e8t",
+                "nine" to "n9e",
+            )
+
+        var string: String
+
+        input.forEach { inputValue ->
+            string = inputValue
+
+            digits.forEach { digit ->
+                string = string.replace(digit.key, digit.value)
+            }
+
+            inputDigits.add(formatValue(string))
+        }
+
+        return inputDigits.sum()
     }
 
-    // test if implementation meets criteria from the description, like:
-    val testInput = readInput("Day01_test")
-    check(part1(testInput) == 1)
+    val testInput1 = readInput("Day01/P1_test")
+    check(part1(testInput1) == 142)
 
-    val input = readInput("Day01")
+    val testInput2 = readInput("Day01/P2_test")
+    check(part2(testInput2) == 281)
+
+    val input = readInput("Day01/P1")
     part1(input).println()
     part2(input).println()
 }
